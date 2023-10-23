f5b756180f36b0e414be53edfb4e40cd3e4b6253
Saulo Henrique Aguiar <saulohjr@hotmail.com>
2023-10-23 18:12:51 -0300

    calculadora gorgeta

---
+++ calculadoragorgeta/scripts.js
@@ -0,0 +1,25 @@
+const calculateBtn = document.querySelector('#calculateBtn');
+
+
+
+function calculateTip() {
+ const billAmount = parseFloat(document.querySelector('#billAmount').value);
+ const serviceQuality = parseFloat(document.querySelector('#serviceQuality').value);
+
+ if(billAmount === '' || serviceQuality == 0) {
+     alert('Por favor, preencha os valores');
+     return;
+ }
+ const tipAmount = billAmount * serviceQuality;
+ const totalAmount = billAmount + tipAmount;
+  
+ const tipAmountInput = document.querySelector('#tipAmount');
+ const totalAmountInput = document.querySelector('#totalAmount');
+
+ tipAmountInput.value = `R$ ${tipAmount}`;
+ totalAmountInput.value =  `R$ ${totalAmount}`;
+
+}
+
+
+calculateBtn.addEventListener('click', calculateTip);
